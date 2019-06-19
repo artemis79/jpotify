9211ebdcbdbfe6432eedf05fd1d404184188cf9c
mahshid rahmani <mahshidrahmani2001@gmail.com>
2019-06-19 09:24:19 +0430

    main to test the song class

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -1,25 +1,32 @@
+package Logic;
+
+import Logic.Song;
 import javazoom.jl.decoder.JavaLayerException;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
 
 
 public class Main {
     public static void main(String[] args) {
         try {
-            Song song = new Song("C:\\Users\\mahsh\\IdeaProjects\\Jpotify\\src\\mysong.mp3");
+            Song song = new Song("C:\\Users\\mahsh\\IdeaProjects\\Jpotify\\src\\Logic\\mysong.mp3");
             song.play();
             Scanner reader = new Scanner(System.in);
             String str;
             while (true){
                 str = reader.next();
-                if (str.equals("pause"))
-                    song.pause();
-                else if (str.equals("resume"))
-                    song.resume();
-                else if (str.equals("terminate")) {
-                    song.stop();
-                    return;
+                switch (str) {
+                    case "pause":
+                        song.pause();
+                        break;
+                    case "resume":
+                        song.resume();
+                        break;
+                    case "terminate":
+                        song.stop();
+                        return;
                 }
             }
         } catch (FileNotFoundException e) {
@@ -31,6 +38,8 @@ public class Main {
         } catch (InterruptedException e) {
             e.printStackTrace();
             System.out.println("could not pause the music");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
 
